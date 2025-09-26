7c1d2bf9e715cc91c5f103741ea0a07b7aff22a2
Fatwah Firmansyah <fatwah091@gmail.com>
2025-09-26 16:03:00 +0700

    apply cors

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -31,6 +32,15 @@ func main() {
 	}
 
 	r := gin.New()
+	r.Use(cors.New(cors.Config{
+		AllowOriginFunc: func(origin string) bool { return true },
+
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	r.Use(gin.Logger(), gin.Recovery())
 
 	jwtm := jwtx.New(cfg.JWTSecret, cfg.JWTTTLMinutes)
